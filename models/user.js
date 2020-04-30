312c29ba7d3f897e49544d7e755e274389da29d9
NTKKS <janx2as@gmail.com>
2020-04-30 16:57:43 +0200

    fixes

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const authorSchema = new mongoose.Schema({
+    /*
     id: {
         type: id,
         unique:true,
@@ -19,6 +20,7 @@ const authorSchema = new mongoose.Schema({
         type: password,
         required:true,
     }
+    */
 })
 
 module.exports = mongoose.model('User', authorSchema)
